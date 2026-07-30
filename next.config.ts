a245276125c2181b2a230cb687ae363637126a31
Void&Null <70048414+Void-n-Null@users.noreply.github.com>
2026-07-30 00:47:29 -0500

    feat: redirect legacy domain to werlinger.dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/projects/godot-goap",
+        destination: "https://werlinger.dev/projects/godot-goap-demo",
+        statusCode: 301,
+      },
+      {
+        source: "/projects/imagine-app",
+        destination: "https://werlinger.dev/projects/imagineapp",
+        statusCode: 301,
+      },
+      {
+        source: "/projects/rebang",
+        destination: "https://werlinger.dev/projects/rebang",
+        statusCode: 301,
+      },
+      {
+        source: "/:path*",
+        destination: "https://werlinger.dev/:path*",
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
